86ff1ed7a5dcd3833d26aad2533b534f0821fcd7
Srinivasarao Mogallapu <cnu4cloud@gmail.com>
2020-08-11 12:49:23 +0530

    derivate type

---
+++ 01basics/01Variables/derivedType.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+type mytype int
+
+func main() {
+	var a mytype = 10.0
+	var b float32 = 10.5
+	fmt.Printf("The value of a and b are %v %v and The type of rescpective vars are %T %T\n", a, b, a, b)
+	//fmt.Println("Is a equals to be?", a == b)
+	//./derivedType.go:13:38: invalid operation: a == b (mismatched types mytype and float32)
+}
