fca20b64dd711cf12bcb367c52104622bd157c34
Srinivasarao Mogallapu <cnu4cloud@gmail.com>
2020-09-14 15:37:40 +0530

    new 2d array code

---
+++ 01basics/05Arrays/multidim_array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [3][2]int{{0, 1}, {1, 0}, {0, 0}}
+	var i, j int
+	for i = 0; i < 3; i++ {
+		for j = 0; j < 2; j++ {
+			fmt.Printf("a[%d][%d] of array is %.1d\n", i, j, a[i][j])
+		}
+
+	}
+}
