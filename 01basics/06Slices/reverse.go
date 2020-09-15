d0f720c77cff487ed941155640525ec3c3201a1b
Srinivasarao Mogallapu <cnu4cloud@gmail.com>
2020-09-15 21:47:14 +0530

    slice code

---
+++ 01basics/06Slices/reverse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sliceOne := []int{1, 2, 3, 4, 5}
+	fmt.Printf("SliceOne elements:  %v\n", sliceOne)
+
+	for _, v1 := range sliceOne {
+		fmt.Printf("%d\n", v1)
+	    i = i++
+	}
+	//reverse
+
+}
