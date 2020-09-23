c7d7183254ab48a7c6f4750795cdeae43a49f3dd
Srinivasarao Mogallapu <49227626+mogallas@users.noreply.github.com>
2020-09-23 07:41:34 +0530

    Update multidim_array.go

--- 01basics/05Arrays/multidim_array.go
+++ 01basics/05Arrays/multidim_array.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := [3][2]int{{0, 1}, {1, 0}, {0, 0}}
+	a := [4][2]int{{0, 1}, {1, 0}, {0, 0}, {3,2}}
 	var i, j int
 	for i = 0; i < 3; i++ {
 		for j = 0; j < 2; j++ {
